ea1edabfbeb453cb50de8f5d966ba3614c675a63
vernierm <verniermt@gmail.com>
2022-01-04 23:15:27 +0100

    make it work

--- src/main/kotlin/per/cleanarchitecture/adapters/MovieController.kt
+++ src/main/kotlin/per/cleanarchitecture/adapters/MovieController.kt
@@ -1,5 +1,6 @@
 package per.cleanarchitecture.adapters
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,8 +11,11 @@ import per.cleanarchitecture.usecases.MovieResponseModel
 
 @RestController
 class MovieController(private val movieBoundary: MovieBoundary) {
+    data class JsonMovieRequestModel @JsonCreator constructor(val name: String)
+
     @PostMapping("/movie")
-    fun create(@RequestBody request: MovieRequestModel): MovieResponseModel = movieBoundary.create(request)
+    fun create(@RequestBody request: JsonMovieRequestModel) =
+            movieBoundary.create(MovieRequestModel(request.name))
 
     @GetMapping("/movies")
     fun getAll(): List<MovieResponseModel> = movieBoundary.getAll()
